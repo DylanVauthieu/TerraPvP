9a90f0cfd5dd9c19fd735afdfcceca752d02280f
DylanVauthieu <dylan.vauthieu@epitech.eu>
2022-11-06 16:48:34 +0100

    Add : Process for generate Ore and Rare or in map

---
+++ Common/Systems/WorldSystem.cs
@@ -0,0 +1,19 @@
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+using System.Collections.Generic;
+using TerraPvP.Common.Systems.GenPasses;
+
+namespace TerraPvP.Common.Systems
+{
+    internal class WorldSystem : ModSystem
+    {
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            int shiniesIndex = tasks.FindIndex(t => t.Name.Equals("Shinies"));
+            if (shiniesIndex != -1)
+            {
+                tasks.Insert(shiniesIndex + 1, new TutorialOreGenPass("Tutorial Ore Pass", 320f));
+            }
+        }
+    }
+}
