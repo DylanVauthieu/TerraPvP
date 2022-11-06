eaea3f068e749844b2cbadaeafec5a00bf277b00
DylanVauthieu <dylan.vauthieu@epitech.eu>
2022-11-06 03:05:57 +0100

    Add : First tiles or ore / Bar

---
+++ Content/Tiles/TutorialOre.cs
@@ -0,0 +1,32 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace TerraPvP.Content.Tiles
+{
+    internal class TutorialOre : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            TileID.Sets.Ore[Type] = true;
+
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileShine[Type] = 900;
+            Main.tileShine2[Type] = true;
+            Main.tileSpelunker[Type] = true;
+            Main.tileOreFinderPriority[Type] = 350;
+
+            AddMapEntry(new Color(200, 200, 200), CreateMapEntryName());
+
+            DustType = DustID.Tungsten;
+            ItemDrop = ModContent.ItemType<Items.Placeables.TutorialOre>();
+            HitSound = SoundID.Tink;
+
+            MineResist = 1.5f;
+            MinPick = 60;
+        }
+    }
+}
