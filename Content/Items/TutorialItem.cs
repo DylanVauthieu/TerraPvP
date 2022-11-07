41710903d899b1577cfc6b4dc3dc64bf7dbd75c0
DylanVauthieu <dylan.vauthieu@epitech.eu>
2022-11-07 02:21:12 +0100

    Change : Renamed

---
+++ Content/Items/TutorialItem.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace TerraPvP.Content.Items
+{
+    internal class TutorialItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+
+            Item.value = Item.buyPrice(copper: 5, silver: 3, gold: 1);
+            Item.maxStack = 10;
+        }
+    }
+}
