8b4e94e45ba49eb2f7ff538720d699ac18e62568
DylanVauthieu <dylan.vauthieu@epitech.eu>
2022-11-20 05:33:54 +0100

    Add : Simple Bloc Added

---
+++ Content/Tiles/ExampleBlock.cs
@@ -0,0 +1,25 @@
+using TerraPvP.Content.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerraPvP.Content.Tiles
+{
+	public class ExampleBlock : ModTile
+	{
+		public override void SetStaticDefaults() {
+			Main.tileSolid[Type] = true;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileBlockLight[Type] = true;
+
+			DustType = ModContent.DustType<Sparkle>();
+			ItemDrop = ModContent.ItemType<Items.Placeable.ExampleBlock>();
+
+			AddMapEntry(new Color(200, 200, 200));
+		}
+
+		public override void NumDust(int i, int j, bool fail, ref int num) {
+			num = fail ? 1 : 3;
+		}
+	}
+}
